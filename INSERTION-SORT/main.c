5db7ff7aa20642606940675af79e7f73e85d812b
sidalei <suchao1991@gmail.com>
2015-05-21 19:36:56 +0800

    Initial Commit

---
+++ INSERTION-SORT/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  INSERTION-SORT
+//
+//  Created by Suchao on 15/5/21.
+//  Copyright (c) 2015年 Suchao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
